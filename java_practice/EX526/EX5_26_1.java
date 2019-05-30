bba6dfe7e85cf97ff1badf1fa5fb00a783306f01
Mosand <974386121@qq.com>
2019-05-30 23:27:49 +0800

    更改了一下package

---
+++ java_practice/EX526/EX5_26_1.java
@@ -0,0 +1,18 @@
+package EX526;
+
+public class EX5_26_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("*********    ***      *        *");
+        System.out.println("*       *   *   *    ***      * *");
+        System.out.println("*       *  *     *  *****    *   *");
+        System.out.println("*       *  *     *    *     *     *");
+        System.out.println("*       *  *     *    *    *       *");
+        System.out.println("*       *  *     *    *     *     *");
+        System.out.println("*       *  *     *    *      *   *");
+        System.out.println("*       *   *   *     *       * *");
+        System.out.println("*********    ***      *        *");
+	}
+
+}
---
+++ java_practice/EX526/EX5_26_5.java
@@ -0,0 +1,12 @@
+package EX526;
+
+public class EX5_26_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0; i<8; ++i){
+            System.out.printf("%s* * * * * * * *\n", (i % 2 != 0 ? " " : ""));
+        }
+	}
+
+}
